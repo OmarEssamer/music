29c56623371f90b724c072632a1a260dd1af0100
OmarEssamer <43882047+OmarEssamer@users.noreply.github.com>
2019-02-28 10:13:33 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -20,7 +20,7 @@ const gif = require("gif-search");
 
 const client = new Discord.Client({disableEveryone: true});
 
-const prefix = "=";
+const prefix = "?";
 /////////////////////////
 ////////////////////////
 
@@ -349,7 +349,7 @@ client.on('ready', () => {
       console.log(`ON ${client.guilds.size} Servers '     Script By : EX Clan ' `);
     console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`=play | ESDream Music`,"http://twitch.tv/Death Shop")
+client.user.setGame(`?play | Kingro Music`,"http://twitch.tv/Death Shop")
 client.user.setStatus("dnd")
 });
 
